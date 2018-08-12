d59ef1c6f094768999f0e2226d328aa6cac78858
Phantom <phantom@phantom.im>
2018-08-12 15:58:36 -0500

    Moved several constants to config file.

--- scripts/config.py
+++ scripts/config.py
@@ -5,10 +5,16 @@ REDDIT_USERNAME = ""
 REDDIT_PASSWORD = ""
 APP_ID = ""
 APP_SECRET = ""
-USER_AGENT = "EmpleosBot v0.1"
+USER_AGENT = "EmpleosBot v0.2"
 
-# The thread that the post_bot will keep updating.
-POST_ID = "93i25l"
+# 0 for local time, 5 for Mexico Central Time.
+DELTA_HOURS = 0
+
+# Seconds in a day multiplied for the number of days.
+JOBS_MAX_AGE = 86400 * 3
+
+# The threads that the post_bot will keep updating.
+POST_IDS = ["93i25l", "93i25l"]
 
 # The threads the comments_bot will keep checking for new comments.
-SUBMISSION_IDS = ["938fpu", "93i25l", "93au4i"]
+SUBMISSION_IDS = ["938fpu", "93i25l", "93au4i", "952ifq"]
